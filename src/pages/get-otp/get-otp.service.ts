878965b66e249c6ac9dc1b33a5fd3296106f5d63
Gourav Malviya <65213810+Gourav-mlv@users.noreply.github.com>
2020-10-07 20:00:38 +0530

    Update get-otp.service.ts

--- src/pages/get-otp/get-otp.service.ts
+++ src/pages/get-otp/get-otp.service.ts
@@ -4,9 +4,6 @@ import { Observable } from 'rxjs/Observable'
 
 @Injectable()
 export class GetOtpService {
-   // baseURL: string = 'https://api.msg91.com/api/v5/otp?authkey=329039AEVFbxMMy9n5ebbdf3fP1&template_id=5ebbdf67d6fc051be8752693&extra_param={}&mobile=';
-  //  baseURL2: string = '&invisible=1&otp=';
-
     baseURl :string= 'http://localhost:3000/app/loginTried';
     //url: 'http://192.168.43.11:3000/test';
     constructor(private http: HttpClient) { }
